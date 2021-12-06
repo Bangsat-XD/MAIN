644f0ea6134493c1549117326494c7ad5bd34d21
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-12-06 16:17:36 +0700

    Update Nano.py

--- Nano.py
+++ Nano.py
@@ -104,11 +104,11 @@ def yayanxd():
     kontol = raw_input('\n %s[%s?%s] ☆Enter Token☆ ™︻®╤───────═◍➤ :%s '%(N,M,N,H))
     if kontol in ('open', 'Open', 'OPEN'):
         print '\n%s ◍➤%s Login Di Kiwi Browser Terlebih Dahulu'%(B,N);time.sleep(2)
-        print '%s *◍➤%s Tutorial Ada Di %YOUTUBE'%(B,N,H);time.sleep(2)
-        print '%s *◍➤%s Simak Baik Baik. klik %stitik tiga'%(B,N,H);time.sleep(2)
-        print '%s *◍➤%s lalu klik %sCari di Halaman%s Tinggal ketik %sEAAA%s Lalu salin.'%(B,N,H,N,H,N);time.sleep(2)
-        raw_input(' %s*?%s ☆Tekan Enter☆ ™︻®╤───────═◍➤ : '%(O,N))
-        os.system('xdg-open https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed#_=_')
+        print '%s ◍➤%s Tutorial Ada Di %YOUTUBE'%(B,N,H);time.sleep(2)
+        print '%s ◍➤%s Simak Baik Baik. klik %stitik tiga'%(B,N,H);time.sleep(2)
+        print '%s ◍➤%s lalu klik %sCari di Halaman%s Tinggal ketik %sEAAA%s Lalu salin.'%(B,N,H,N,H,N);time.sleep(2)
+        raw_input(' [%s?%s] ☆Tekan Enter☆ ™︻®╤───────═◍➤ : '%(O,N))
+        os.system('xdg-open https://youtu.be/bszAm4C5ovE')
         yayanxd()
     try:
         nama = requests.get('https://graph.facebook.com/me?access_token=%s'%(kontol)).json()['name']
@@ -136,7 +136,6 @@ def moch_yayan():
         exit('\n\n %s[%s!%s] tidak ada koneksi\n'%(N,M,N))
     os.system('clear')
     print logo
-    IP = requests.get('https://www.yayanxd.my.id/server/ip/').text
     print '___________________________________________________________\n';time.sleep(0.03)
     print ' (\033[0;96m•\033[0m) ACTIVE USER : %s'%(nama);time.sleep(0.03)
     print ' (\033[0;96m•\033[0m) IP DEVICE   : %s'%(IP)
