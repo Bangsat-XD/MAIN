2f910a2aff470e1f076533d14f2b7baa5fea2427
☆ Raka ☆ ™︻®╤───────═◍➤ <93072215+Bangsat-XD@users.noreply.github.com>
2021-12-06 15:50:14 +0700

    Update Nano.py

--- Nano.py
+++ Nano.py
@@ -83,9 +83,9 @@ def tod():
 
 # LO KONTOL
 logo = ''' \033[0;96m __  __        __  ______  ____
- \033[0;96m \ \/ / ____  /  |/  / _ )/ __/ ® \033[0m|| Created By YayanXD
- \033[0;96m  \  / /___/ / /|_/ / _  / _/     \033[0m|| Github.com/Yayan-XD
- \033[0;96m  /_/       /_/  /_/____/_/ \033[0;91mv2.0  \033[0m|| Facebook.com/KM39453'''
+ \033[0;96m \ \/ / ____  /  |/  / _ )/ __/ ® \033[0m|| Created By Angga
+ \033[0;96m  \  / /___/ / /|_/ / _  / _/     \033[0m|| Github.com/Bajingan-Z
+ \033[0;96m  /_/       /_/  /_/____/_/ \033[0;91mv2.0  \033[0m|| Fb.me/PEMUDA.KALEUM'''
 
 lo_ngentod = '1714009362122228'
 # crack selesai
